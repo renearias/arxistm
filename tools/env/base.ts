b9172643691f1231f2ed34940b9f5d9912ea2010
renearias <rene_811@hotmail.com>
2016-10-30 18:08:39 -0500

    update base config

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -2,7 +2,9 @@ import { EnvConfig } from './env-config.interface';
 
 const BaseConfig: EnvConfig = {
   // Sample API url
-  API: 'https://demo.com'
+  API: 'http://localhost/arxisTMApi/web/app_dev.php/',
+  OAUTH_CLIENT_ID: '1_2fq6ri2xx4cgc8o8kgwwwgc0gk040cwwoww0k4gc0oksksgk0g',
+  OAUTH_CLIENT_SECRET: '5inlap0v5pgkg8co8o84gocwwk8wo84woskk08w08o08ooc804'
 };
 
 export = BaseConfig;
