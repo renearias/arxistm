ce380eac97a6c9f544d5f3bd7cb853a2a3c55100
renearias <rene_811@hotmail.com>
2016-10-30 20:20:20 -0500

    espaniol

---
+++ src/client/app/directives/dynamicDataTable/translations/es-ES.ts
@@ -0,0 +1,39 @@
+export const DataTableLangEsES: any = {
+    'processing': 'Procesando...',
+    'lengthMenu': 'Ver _MENU_ resultados',
+    'sLengthMenu': '_MENU_',
+    'zeroRecords': 'Ningún resultado encontrado',
+    'info': 'Mostrando _START_ de _END_ de un total de _TOTAL_ resultados',
+    'sInfo': 'Mostrando <strong>_START_</strong> a <strong>_END_</strong> de <strong><span class="text-primary">_TOTAL_</span></strong> registros',
+    'infoEmpty': 'Mostrando 0 de 0 de un total de 0 resultados',
+    'sInfoEmpty': '<span class="text-danger">Mostrando 0 a 0 de 0 registros</span>',
+    'infoFiltered': '(filtrado de un total de _MAX_ resultados)',
+    'infoPostFix': '',
+    'search': '<span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>',
+    'sSearch': 'Buscar: ',
+    'oPaginate': {
+        'sFirst': 'Primero',
+        'sLast': 'Último',
+        'sNext': 'Siguiente',
+        'sPrevious': 'Anterior'
+    },
+    'all': 'Todos',
+    'sAll': 'Todos',
+    'oAria': {
+        'sSortAscending': ': Activar para ordenar la columna de manera ascendente',
+        'sSortDescending': ': Activar para ordenar la columna de manera descendente'
+    },
+    'bulk': {
+        'confirmMsg': '¿Está seguro?',
+        'selectError': 'Es necesario seleccionar al menos 1 elemento.',
+        'ok': 'Aplicar'
+    },
+    'buttons': {
+        'copyTitle': 'Copiado al portapapeles',
+        'copyKeys': 'Presione <i>ctrl</i> o <i>\u2318</i> + <i>C</i> para copiar datos de la tabla en el portapapeles. <br><br>Para cancelar, click fuera del mensaje o precione Esc.',
+        'copySuccess': {
+            '_': 'Copiadas %d filas al portapapeles',
+            '1': 'Copiada 1 fila al portapapeles'
+        }
+    }
+};
