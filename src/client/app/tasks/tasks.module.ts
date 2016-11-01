de2bb8c2d09ff6c2f27f89fa6f7d34921d0a8214
renearias <rene_811@hotmail.com>
2016-11-01 13:28:43 -0500

    import shared module

--- src/client/app/tasks/tasks.module.ts
+++ src/client/app/tasks/tasks.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 import { TasksComponent } from './tasks.component';
 import { TasksService } from './tasks.service';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, SharedModule],
     declarations: [TasksComponent],
     providers: [TasksService],
     exports: [TasksComponent]
