d99a80573bf403025ae340ac72bd391a99e4a39c
renearias <rene_811@hotmail.com>
2016-11-01 14:20:15 -0500

    update

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 import { NameListService } from '../shared/name-list/index';
-import { DynamicDataTable } from '../directives/index'
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [HomeComponent, DynamicDataTable],
+  declarations: [HomeComponent],
   exports: [HomeComponent],
   providers: [NameListService]
 })
