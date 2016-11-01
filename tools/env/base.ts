f736f35aef82eedd6dbd20d0e70f57c9997e949e
renearias <rene_811@hotmail.com>
2016-10-31 22:21:47 -0500

    uptate prod api

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -2,7 +2,7 @@ import { EnvConfig } from './env-config.interface';
 
 const BaseConfig: EnvConfig = {
   // Sample API url
-  API: 'http://localhost/arxisTMApi/web/app_dev.php/',
+  API: 'https://arxistmapi.herokuapp.com/',
   OAUTH_CLIENT_ID: '1_2fq6ri2xx4cgc8o8kgwwwgc0gk040cwwoww0k4gc0oksksgk0g',
   OAUTH_CLIENT_SECRET: '5inlap0v5pgkg8co8o84gocwwk8wo84woskk08w08o08ooc804'
 };
